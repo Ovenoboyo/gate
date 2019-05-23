e0e336c8b3f4b581e0bb7bcda76a40f48fb10bc8
Ovenoboyo <ovenoboyo@gmail.com>
2019-05-23 22:14:34 +0530

    Add gate-admin package

---
+++ gate_admin/gate_admin/lib/pages/home_page.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'dart:async';
+import 'login_signup_page.dart';
+import 'package:gate_admin/services/authentication.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key, this.auth, this.userId, this.onSignedOut})
+      : super(key: key);
+
+  final BaseAuth auth;
+  final VoidCallback onSignedOut;
+  final String userId;
+
+  @override
+  State<StatefulWidget> createState() => new _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool _isEmailVerified = false;
+
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  void _checkEmailVerification() async {
+    _isEmailVerified = await widget.auth.isEmailVerified();
+    if (!_isEmailVerified) {
+      _showVerifyEmailDialog();
+    }
+  }
+
+
+
+  void _resentVerifyEmail(){
+    widget.auth.sendEmailVerification();
+    _showVerifyEmailSentDialog();
+  }
+
+  void _showVerifyEmailDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        // return object of type Dialog
+        return AlertDialog(
+          title: new Text("Verify your account"),
+          content: new Text("Please verify account in the link sent to email"),
+          actions: <Widget>[
+            new FlatButton(
+              child: new Text("Resent link"),
+              onPressed: () {
+                Navigator.of(context).pop();
+                _resentVerifyEmail();
+              },
+            ),
+            new FlatButton(
+              child: new Text("Dismiss"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showVerifyEmailSentDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        // return object of type Dialog
+        return AlertDialog(
+          title: new Text("Verify your account"),
+          content: new Text("Link to verify account has been sent to your email"),
+          actions: <Widget>[
+            new FlatButton(
+              child: new Text("Dismiss"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void onRegisterUser() {
+    Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) {
+      return new LoginSignUpPage(auth: widget.auth, onSignedIn: _onSignedup, formMode: FormMode.SIGNUP, flatUser: true,);
+    })
+    );
+  }
+
+  void _onSignedup() {
+
+  }
+  
+  _signOut() async {
+    try {
+      await widget.auth.signOut();
+      widget.onSignedOut();
+    } catch (e) {
+      print(e);
+    }
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    _checkEmailVerification();
+    return new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Flutter login demo'),
+          actions: <Widget>[
+            new FlatButton(
+                child: new Text('Logout',
+                    style: new TextStyle(fontSize: 17.0, color: Colors.white)),
+                onPressed: _signOut)
+          ],
+        ),
+        body: new Center(
+          child: new RaisedButton(onPressed: onRegisterUser),
+        )
+    );
+  }
+}
---
+++ gate_admin/gate_admin/lib/pages/login_signup_page.dart
@@ -0,0 +1,291 @@
+import 'package:flutter/material.dart';
+import 'package:gate_admin/services/authentication.dart';
+import 'package:firebase_database/firebase_database.dart';
+
+class LoginSignUpPage extends StatefulWidget {
+  LoginSignUpPage({this.auth, this.onSignedIn, this.formMode, this.flatUser});
+
+  final BaseAuth auth;
+  final VoidCallback onSignedIn;
+  final FormMode formMode;
+  final bool flatUser;
+
+  @override
+  State<StatefulWidget> createState() => new _LoginSignUpPageState();
+}
+
+enum FormMode { LOGIN, SIGNUP }
+
+class _LoginSignUpPageState extends State<LoginSignUpPage> {
+  final _formKey = new GlobalKey<FormState>();
+  final databaseReference = FirebaseDatabase.instance.reference();
+
+  String _email;
+  String _password;
+  String _errorMessage;
+  String _wing, _house;
+
+  FormMode _formMode;
+  bool _isIos;
+  bool _isLoading;
+
+  // Check if form is valid before perform login or signup
+  bool _validateAndSave() {
+    final form = _formKey.currentState;
+    if (form.validate()) {
+      form.save();
+      return true;
+    }
+    return false;
+  }
+
+  // Perform login or signup
+  void _validateAndSubmit() async {
+    setState(() {
+      _errorMessage = "";
+      _isLoading = true;
+    });
+    if (_validateAndSave()) {
+      String userId = "";
+      try {
+        if (_formMode == FormMode.LOGIN) {
+          userId = await widget.auth.signIn(_email, _password);
+          print('Signed in: $userId');
+        } else {
+          userId = await widget.auth.signUp(_email, _password);
+          widget.auth.sendEmailVerification();
+          _showVerifyEmailSentDialog();
+          print('Signed up user: $userId');
+          if (widget.flatUser) {
+            String address = _wing.toUpperCase()+"-"+_house;
+            databaseReference
+                .child("FlatAssociates").child(address)
+                .set({'Name': userId});
+          }
+        }
+        setState(() {
+          _isLoading = false;
+        });
+
+        if (userId.length > 0 && userId != null && _formMode == FormMode.LOGIN) {
+          widget.onSignedIn();
+        }
+
+      } catch (e) {
+        print('Error: $e');
+        setState(() {
+          _isLoading = false;
+          if (_isIos) {
+            _errorMessage = e.details;
+          } else
+            _errorMessage = e.message;
+        });
+      }
+    }
+  }
+
+
+  @override
+  void initState() {
+    _formMode = widget.formMode;
+    _errorMessage = "";
+    _isLoading = false;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _isIos = Theme.of(context).platform == TargetPlatform.iOS;
+    return new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Flutter login demo'),
+        ),
+        body: Stack(
+          children: <Widget>[
+            _showBody(),
+            _showCircularProgress(),
+          ],
+        ));
+  }
+
+  Widget _showCircularProgress(){
+    if (_isLoading) {
+      return Center(child: CircularProgressIndicator());
+    } return Container(height: 0.0, width: 0.0,);
+
+  }
+
+  void _showVerifyEmailSentDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        // return object of type Dialog
+        return AlertDialog(
+          title: new Text("Verify your account"),
+          content: new Text("Link to verify account has been sent to your email"),
+          actions: <Widget>[
+            new FlatButton(
+              child: new Text("Dismiss"),
+              onPressed: null,
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Widget _showBody(){
+    return new Container(
+        padding: EdgeInsets.all(16.0),
+        child: new Form(
+          key: _formKey,
+          child: new ListView(
+            shrinkWrap: true,
+            children: <Widget>[
+              _showLogo(),
+              _showEmailInput(),
+              new Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  new Flexible(child: _showWingInput()),
+                  new Flexible(child: _showHouseInput())
+                ],
+              ),
+              _showPasswordInput(),
+              _showPrimaryButton(),
+              _showErrorMessage(),
+            ],
+          ),
+        ));
+  }
+
+  Widget _showErrorMessage() {
+    if (_errorMessage.length > 0 && _errorMessage != null) {
+      return new Text(
+        _errorMessage,
+        style: TextStyle(
+            fontSize: 13.0,
+            color: Colors.red,
+            height: 1.0,
+            fontWeight: FontWeight.w300),
+      );
+    } else {
+      return new Container(
+        height: 0.0,
+      );
+    }
+  }
+
+  Widget _showLogo() {
+    return new Hero(
+      tag: 'hero',
+      child: Padding(
+        padding: EdgeInsets.fromLTRB(0.0, 70.0, 0.0, 0.0),
+        child: CircleAvatar(
+          backgroundColor: Colors.transparent,
+          radius: 48.0,
+          child: Image.asset('assets/flutter-icon.png'),
+        ),
+      ),
+    );
+  }
+
+  Widget _showWingInput() {
+    if (widget.flatUser) {
+      return Padding(
+        padding: const EdgeInsets.fromLTRB(0.0, 15.0, 0.0, 0.0),
+        child: new TextFormField(
+          maxLines: 1,
+          keyboardType: TextInputType.emailAddress,
+          autofocus: false,
+          decoration: new InputDecoration(
+              hintText: 'Wing',
+              icon: new Icon(
+                Icons.hotel,
+                color: Colors.grey,
+              )),
+          validator: (value) => value.isEmpty ? 'Wing can\'t be empty' : null,
+          onSaved: (value) => _wing = value,
+        ),
+      );
+    } else {
+      return new Container();
+    }
+  }
+
+  Widget _showHouseInput() {
+    if (widget.flatUser) {
+      return Padding(
+        padding: const EdgeInsets.fromLTRB(0.0, 15.0, 0.0, 0.0),
+        child: new TextFormField(
+          maxLines: 1,
+          keyboardType: TextInputType.number,
+          autofocus: false,
+          decoration: new InputDecoration(
+              hintText: 'House Number'),
+          validator: (value) => value.isEmpty ? 'Wing can\'t be empty' : null,
+          onSaved: (value) => _house = value,
+        ),
+      );
+    } else {
+      return new Container();
+    }
+  }
+
+  Widget _showEmailInput() {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(0.0, 100.0, 0.0, 0.0),
+      child: new TextFormField(
+        maxLines: 1,
+        keyboardType: TextInputType.emailAddress,
+        autofocus: false,
+        decoration: new InputDecoration(
+            hintText: 'Email',
+            icon: new Icon(
+              Icons.mail,
+              color: Colors.grey,
+            )),
+        validator: (value) => value.isEmpty ? 'Email can\'t be empty' : null,
+        onSaved: (value) => _email = value,
+      ),
+    );
+  }
+
+  Widget _showPasswordInput() {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(0.0, 15.0, 0.0, 0.0),
+      child: new TextFormField(
+        maxLines: 1,
+        obscureText: true,
+        autofocus: false,
+        decoration: new InputDecoration(
+            hintText: 'Password',
+            icon: new Icon(
+              Icons.lock,
+              color: Colors.grey,
+            )),
+        validator: (value) => value.isEmpty ? 'Password can\'t be empty' : null,
+        onSaved: (value) => _password = value,
+      ),
+    );
+  }
+
+  Widget _showPrimaryButton() {
+    return new Padding(
+        padding: EdgeInsets.fromLTRB(0.0, 45.0, 0.0, 0.0),
+        child: SizedBox(
+          height: 40.0,
+          child: new RaisedButton(
+            elevation: 5.0,
+            shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
+            color: Colors.blue,
+            child: _formMode == FormMode.LOGIN
+                ? new Text('Login',
+                    style: new TextStyle(fontSize: 20.0, color: Colors.white))
+                : new Text('Create account',
+                    style: new TextStyle(fontSize: 20.0, color: Colors.white)),
+            onPressed: _validateAndSubmit,
+          ),
+        ));
+  }
+}
