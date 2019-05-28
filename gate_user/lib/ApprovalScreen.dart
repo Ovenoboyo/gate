67c70c921c4fc9b5e5c4bc7a00d905b471d743a2
Ovenoboyo <ovenoboyo@gmail.com>
2019-05-28 17:45:28 +0530

    Add a swipe button by @slightfoot

--- gate_user/lib/ApprovalScreen.dart
+++ gate_user/lib/ApprovalScreen.dart
@@ -4,6 +4,8 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'dart:io';
 import 'package:gate_user/main.dart';
+import 'package:gate_user/pages/home_page.dart';
+import 'package:gate_user/model/swipe_button.dart';
 
 class ApprovalScreen extends StatefulWidget {
 
@@ -28,9 +30,15 @@ class ApprovalScreenState extends State<ApprovalScreen> {
   final databaseReference = FirebaseDatabase.instance.reference();
   FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = new FlutterLocalNotificationsPlugin();
 
+  Color firstColor = Colors.blue;
+  Color secondColor = Colors.blue[400];
+
   @override
-  void initState()  {
-    databaseReference.child("pendingApproval").onChildRemoved.listen((Event event) {
+  void initState() {
+    databaseReference
+        .child("pendingApproval")
+        .onChildRemoved
+        .listen((Event event) {
       moveScreens();
     });
     super.initState();
@@ -41,16 +49,17 @@ class ApprovalScreenState extends State<ApprovalScreen> {
   }
 
   Future<bool> childExists() async {
-    var data = await databaseReference.child("pendingApproval").child(widget.time).once();
-      if (data.value == null) {
-        return false;
-      } else {
-        return true;
-      }
+    var data = await databaseReference.child("pendingApproval").child(
+        widget.time).once();
+    if (data.value == null) {
+      return false;
+    } else {
+      return true;
+    }
   }
 
   moveScreens() async {
-    if(await childExists() == false ) {
+    if (await childExists() == false) {
       Navigator.of(context).pop();
     }
   }
@@ -59,7 +68,6 @@ class ApprovalScreenState extends State<ApprovalScreen> {
     addNotificationRequest("Approved");
     cancelNotifs();
     Navigator.of(context).pop();
-
   }
 
   void _onPressedDecline() {
@@ -69,7 +77,10 @@ class ApprovalScreenState extends State<ApprovalScreen> {
   }
 
   void addNotificationRequest(String status) {
-    String time = DateTime.now().millisecondsSinceEpoch.toString();
+    String time = DateTime
+        .now()
+        .millisecondsSinceEpoch
+        .toString();
     databaseReference
         .child("notificationRequests")
         .child("to")
@@ -83,41 +94,67 @@ class ApprovalScreenState extends State<ApprovalScreen> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: AppBar(
-        title: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Center(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text('Approve'),
+        appBar: new AppBar(
+          title: new Text('Approve Entry'),
+        ),
+        body: Stack(
+          children: <Widget>[
+            ClipPath(
+              clipper: CustomShapeClipper(),
+              child: Container(
+                height: 400.0,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [firstColor, secondColor],
+                  ),
+                ),
               ),
             ),
-          ),
-        ),
-      ),
-      body: Center(
-        child: new Column(
-          children: <Widget>[
-            new Container(
-              margin: EdgeInsets.only(left: 10, right: 10, top: 20, bottom: 10),
-              child: new Text(widget.flat,  style: new TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
-            ),
-            new Container(
-              margin: EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 7),
-              child: new Text(widget.name+" is requesting approval"),
-            ),
+            Center(
+                child: new Column(
+                  children: <Widget>[
+                    new Container(
+                      margin: EdgeInsets.only(
+                          left: 10, right: 10, top: 20, bottom: 10),
+                      child: new Text(widget.flat, style: new TextStyle(
+                          fontSize: 22, fontWeight: FontWeight.bold, color: Colors.white)),
+                    ),
+                    new Container(
+                      margin: EdgeInsets.only(
+                          left: 10, right: 10, top: 10, bottom: 50),
+                      child: new Text(widget.name + " is requesting approval", style: new TextStyle(color: Colors.white)),
+                    ),
 
-            new Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                new RaisedButton(child: new Text("Accept"), onPressed: _onPressedAccept),
-                new RaisedButton(onPressed: _onPressedDecline, child: new Text("Decline"),)
-              ],
+                    new Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        Expanded(
+                        child: SwipeButton(
+                          thumb: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Align(widthFactor: 0.33, child: Icon(Icons.radio_button_unchecked)),
+                            ],
+                          ),
+                          content: Center(
+                            child: Text(''),
+                          ),
+                          onChanged: (result) {
+                            if (result == SwipePosition.SwipeLeft) {
+                              _onPressedDecline();
+                            } else if( result == SwipePosition.SwipeRight) {
+                              _onPressedAccept();
+                            }
+                          },
+                        ),
+                        )
+                      ],
+                    )
+                  ],
+                )
             )
+
           ],
-        )
-      )
-    );
+        ));
   }
 }
